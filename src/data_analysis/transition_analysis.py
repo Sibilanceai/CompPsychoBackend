6b6e209d1a8f7233299bae11c2d1505f1c76f76c
Ajith Senthil <ajithksenthil@gmail.com>
2024-04-23 12:06:18 -0500

    adding support for all blocks

--- src/data_analysis/transition_analysis.py
+++ src/data_analysis/transition_analysis.py
@@ -12,8 +12,35 @@ from scipy.stats import ttest_ind
 import csv
 
 # Construct the path relative to transition_analysis.py
-med_context_sentences_path = '../data_collection/med_context_sentences.csv'
-med_context_tuples_path = '../data_collection/med_context_tuples.csv'
+
+
+# long_high_sentences_path = '../data_collection/long_high_sentences.csv'
+# long_high_tuples_path = '../data_collection/long_high_tuples.csv'
+
+# med_high_sentences_path = '../data_collection/med_high_sentences.csv'
+# med_high_tuples_path = '../data_collection/med_high_tuples.csv'
+
+# short_high_sentences_path = '../data_collection/short_high_sentences.csv'
+# short_high_tuples_path = '../data_collection/short_high_tuples.csv'
+
+# long_context_sentences_path = '../data_collection/long_context_sentences.csv'
+# long_context_tuples_path = '../data_collection/long_context_tuples.csv'
+
+# med_context_sentences_path = '../data_collection/med_context_sentences.csv'
+# med_context_tuples_path = '../data_collection/med_context_tuples.csv'
+
+# short_context_sentences_path = '../data_collection/short_context_sentences.csv'
+# short_context_tuples_path = '../data_collection/short_context_tuples.csv'
+
+# long_task_sentences_path = '../data_collection/long_task_sentences.csv'
+# long_task_tuples_path = '../data_collection/long_task_tuples.csv'
+
+# med_task_sentences_path = '../data_collection/med_task_sentences.csv'
+# med_task_tuples_path = '../data_collection/med_task_tuples.csv'
+
+# short_task_sentences_path = '../data_collection/short_task_sentences.csv'
+# short_task_tuples_path = '../data_collection/short_task_tuples.csv'
+
 
 def get_file_path(hierarchy, temporality):
     return f'../data_collection/events_{hierarchy.replace(" ", "_")}_{temporality.replace(" ", "_")}.csv'
@@ -26,27 +53,71 @@ def read_csv_to_list(file_path):
 
 
 
-# Read the event sentences and tuples
-med_context_sentences = read_csv_to_list(med_context_sentences_path)
-med_context_tuples = read_csv_to_list(med_context_tuples_path)
-# Flatten the list of lists if necessary
-med_context_sentences = [item for sublist in med_context_sentences for item in sublist]
-med_context_tuples = [item for sublist in med_context_tuples for item in sublist]
+# # Read the event sentences and tuples
+# med_context_sentences = read_csv_to_list(med_context_sentences_path)
+# med_context_tuples = read_csv_to_list(med_context_tuples_path)
+# # Flatten the list of lists if necessary
+# med_context_sentences = [item for sublist in med_context_sentences for item in sublist]
+# med_context_tuples = [item for sublist in med_context_tuples for item in sublist]
 
-def load_events(hierarchy_levels, temporality_levels):
+# Function to combine BC and PS predictions into overall state
+def combine_predictions(predicted_labels_BC, predicted_labels_PS):
+    combined_states = []
+    for bc, ps in zip(predicted_labels_BC, predicted_labels_PS):
+        if bc == 0 and ps == 0:
+            combined_states.append('BP')
+        elif bc == 0 and ps == 1:
+            combined_states.append('BS')
+        elif bc == 1 and ps == 0:
+            combined_states.append('CP')
+        else:  # bc == 1 and ps == 1
+            combined_states.append('CS')
+    return combined_states
+
+def load_events_from_csv(sentences_path, tuples_path):
+    """Loads events from specified CSV files for sentences and tuples."""
+    sentences = read_csv_to_list(sentences_path)
+    tuples = read_csv_to_list(tuples_path)
+    return sentences, tuples
+
+def load_and_classify_events():
     events = {}
-    for hierarchy in hierarchy_levels:
-        for temporality in temporality_levels:
-            file_path = get_file_path(hierarchy, temporality)
-            unprocessedeventlist = read_csv_to_list(file_path)
-            event_list = [item for sublist in unprocessedeventlist for item in sublist]
-            events[(hierarchy, temporality)] = event_list
-    return events
+    # Define paths for each hierarchical and temporal category
+    paths = {
+        ('high-level', 'long-term'): ('../data_collection/long_high_sentences.csv', '../data_collection/long_high_tuples.csv'),
+        ('high-level', 'medium-term'): ('../data_collection/med_high_sentences.csv', '../data_collection/med_high_tuples.csv'),
+        ('high-level', 'short-term'): ('../data_collection/short_high_sentences.csv', '../data_collection/short_high_tuples.csv'),
+        ('context-specific', 'long-term'): ('../data_collection/long_context_sentences.csv', '../data_collection/long_context_tuples.csv'),
+        ('context-specific', 'medium-term'): ('../data_collection/med_context_sentences.csv', '../data_collection/med_context_tuples.csv'),
+        ('context-specific', 'short-term'): ('../data_collection/short_context_sentences.csv', '../data_collection/short_context_tuples.csv'),
+        ('task-specific', 'long-term'): ('../data_collection/long_task_sentences.csv', '../data_collection/long_task_tuples.csv'),
+        ('task-specific', 'medium-term'): ('../data_collection/med_task_sentences.csv', '../data_collection/med_task_tuples.csv'),
+        ('task-specific', 'short-term'): ('../data_collection/short_task_sentences.csv', '../data_collection/short_task_tuples.csv'),
+    }
+    
+    # Load events for each combination of hierarchy and temporality
+    for (hierarchy, temporality), (sentences_path, tuples_path) in paths.items():
+        sentences, tuples = load_events_from_csv(sentences_path, tuples_path)
+        temporal_hierarchy_sentences = [item for sublist in sentences for item in sublist]
+        temporal_hierarchy_tuples = [item for sublist in tuples for item in sublist]
+        predicted_labels_BC_sent, predicted_labels_PS_sent = classify_new_events(temporal_hierarchy_sentences, proto_net_BC, proto_net_PS, prototype_tensor_BC, prototype_tensor_PS)
+        predicted_labels_BC_tup, predicted_labels_PS_tup = classify_new_events(temporal_hierarchy_tuples, proto_net_BC, proto_net_PS, prototype_tensor_BC, prototype_tensor_PS)
+        combined_states_all_sentences = combine_predictions(predicted_labels_BC_sent, predicted_labels_PS_sent)
+        combined_states_all_tuples = combine_predictions(predicted_labels_BC_tup, predicted_labels_PS_tup)
+        # Store the classified events
+        events[(hierarchy, temporality)] = {
+            'sentences': temporal_hierarchy_sentences,
+            'tuples': temporal_hierarchy_tuples,
+            'combined_predicted_labels_sentences': combined_states_all_sentences,
+            'combined_predicted_labels_tuples': combined_states_all_tuples
+        }
+    return events, combined_states_all_sentences, combined_states_all_tuples
+
 
 
 # Assuming the ProtoNet model is already trained or loaded
-predicted_labels_BC_med_context_sent, predicted_labels_PS_med_context_sent = classify_new_events(med_context_sentences, proto_net_BC, proto_net_PS, prototype_tensor_BC, prototype_tensor_PS)
-predicted_labels_BC_med_context_tup, predicted_labels_PS_med_context_tup = classify_new_events(med_context_tuples, proto_net_BC, proto_net_PS, prototype_tensor_BC, prototype_tensor_PS)
+# predicted_labels_BC_med_context_sent, predicted_labels_PS_med_context_sent = classify_new_events(med_context_sentences, proto_net_BC, proto_net_PS, prototype_tensor_BC, prototype_tensor_PS)
+# predicted_labels_BC_med_context_tup, predicted_labels_PS_med_context_tup = classify_new_events(med_context_tuples, proto_net_BC, proto_net_PS, prototype_tensor_BC, prototype_tensor_PS)
 
 
 # TODO need to make functions to make these 
@@ -66,6 +137,10 @@ bc_ps_to_state = {
     (1, 0): 'CP',  # (Consume, Play)
     (1, 1): 'CS',  # (Consume, Sleep)
 }
+
+def combine_predictions(bc_label, ps_label):
+    return bc_ps_to_state.get((bc_label, ps_label))
+
 
 # Initialize your transition matrices dictionary for each character as before
 """
@@ -116,11 +191,7 @@ def save_transition_matrices(matrices, filepath):
 def load_transition_matrices(filepath):
     with open(filepath, 'rb') as f:
         return np.load(f, allow_pickle=True).item()
-# hierarchical/temporal indices and cognitive vectors
-def map_event_to_indices(event):
-    # Might not need
-    return None
-    return temporal_index, hierarchical_index, prev_vector, next_vector
+
 
 def categorize_event_with_langchain(event_description):
     # Interaction with LangChain to categorize the event
@@ -140,16 +211,6 @@ def update_transition_matrix(subject, hierarchy, temporality, prev_state, next_s
         # Update the specified matrix
         transition_matrices[subject][hierarchy][temporality][prev_vector, next_vector] += 1
 
-def process_events_for_all_matrices(characters, hierarchy_levels, temporality_levels, events, transition_matrices):
-    for hierarchy, temporality in events:
-        event_list = events[(hierarchy, temporality)]
-        for event in event_list:
-            subject = get_subject_from_event(event)
-            if subject in characters:
-                prev_state, next_state = extract_states_from_event(event)  # Implement this
-                update_transition_matrix(subject, hierarchy, temporality, prev_state, next_state, transition_matrices, cog_vectors)
-
-
 
 def find_stationary_distribution(transition_matrix):
     eigenvalues, eigenvectors = np.linalg.eig(transition_matrix.T)
@@ -157,28 +218,9 @@ def find_stationary_distribution(transition_matrix):
     stationary_distribution = stationary / stationary.sum()
     return stationary_distribution.real.flatten()
 
-# Iterate over matrices to calculate stationary distributions
-# for hierarchy_level, temporal_matrices in transition_matrices.items():
-#     for temporal_level, matrix in temporal_matrices.items():
-#         dist = find_stationary_distribution(matrix)
-#         # Store or process the stationary distribution
-
-
-
 
 def flatten_matrices(matrices):
     return [matrix.flatten() for matrix in matrices]
-
-# Assuming you've collected all matrices into a list for clustering
-# all_matrices = flatten_matrices([...])  # Flatten all matrices for clustering
-# kmeans = KMeans(n_clusters=3).fit(all_matrices)
-# labels = kmeans.labels_
-
-
-# Pseudo-code, as implementation depends on the specific patterns you're looking for
-# for sequence in sequences:
-#     analyze_sequence(sequence)
-#     # Implement pattern recognition or sequence mining based on your criteria
 
 
 def simulate_behavior(start_state, transition_matrix, num_steps):
@@ -190,103 +232,98 @@ def simulate_behavior(start_state, transition_matrix, num_steps):
     return states
 
 
+def process_and_update_matrices(events_dict, transition_matrices):
+    for (hierarchy, temporality), data in events_dict.items():
+        sentences, tuples = data['sentences'], data['tuples']
+        predicted_labels_BC_sentences, predicted_labels_PS_sentences = data['predicted_labels_BC_sentences'], data['predicted_labels_PS_sentences']
+        predicted_labels_BC_tuples, predicted_labels_PS_tuples = data['predicted_labels_BC_tuples'], data['predicted_labels_PS_tuples']
 
-# set up the transition matrices
+        # Process Sentences
+        for i in range(len(sentences) - 1):  # Ensure there is a next state
+            subject = get_subject_from_event(sentences[i])
+            prev_state = combine_predictions(predicted_labels_BC_sentences[i], predicted_labels_PS_sentences[i])
+            next_state = combine_predictions(predicted_labels_BC_sentences[i+1], predicted_labels_PS_sentences[i+1])
 
+            update_transition_matrix(subject, hierarchy, temporality, prev_state, next_state, transition_matrices, cog_vectors)
+
+        # Process Tuples in the same manner if needed
+        for i in range(len(tuples) - 1):
+            subject = get_subject_from_event(tuples[i])
+            prev_state = combine_predictions(predicted_labels_BC_tuples[i], predicted_labels_PS_tuples[i])
+            next_state = combine_predictions(predicted_labels_BC_tuples[i+1], predicted_labels_PS_tuples[i+1])
+
+            update_transition_matrix(subject, hierarchy, temporality, prev_state, next_state, transition_matrices, cog_vectors)
+
+
+
+
+# Load all events and their predictions
+event_dict, pred_labels_sent, pred_labels_tups = load_and_classify_events()
+
+# Initialize your transition matrices
 transition_matrices = initialize_transition_matrices(characters, hierarchy_levels, temporality_levels, NUM_COG_VECTORS)
-# After updating transition matrices with new event data
-normalize_matrices(transition_matrices)
+print(transition_matrices)
+# Process events and update matrices
+process_and_update_matrices(event_dict, transition_matrices)
 
-# Saving the matrices to a file for persistence
+# Normalize and save matrices
+normalize_matrices(transition_matrices)
 save_transition_matrices(transition_matrices, 'transition_matrices.npy')
 
-# At a later point, to load the matrices
-transition_matrices = load_transition_matrices('transition_matrices.npy')
-
-
-# Function to combine BC and PS predictions into overall state
-def combine_predictions(predicted_labels_BC, predicted_labels_PS):
-    combined_states = []
-    for bc, ps in zip(predicted_labels_BC, predicted_labels_PS):
-        if bc == 0 and ps == 0:
-            combined_states.append('BP')
-        elif bc == 0 and ps == 1:
-            combined_states.append('BS')
-        elif bc == 1 and ps == 0:
-            combined_states.append('CP')
-        else:  # bc == 1 and ps == 1
-            combined_states.append('CS')
-    return combined_states
 
 
 
-# Generate combined states for all sentences, with actual data
-# combined_states_all_sentences = combine_predictions(predicted_labels_BC_med_context_sent, predicted_labels_PS_med_context_sent)
-
-# synthetic data for testing
-import random
-
-def generate_combined_states(num_events, states):
-    """ Generate a random sequence of states for a given number of events. """
-    return [random.choice(states) for _ in range(num_events)]
-
-# Example states and number of events
-states = ['BP', 'BS', 'CP', 'CS']
-num_events = 100  # Number of transitions to simulate
-
-combined_states_all_sentences = generate_combined_states(num_events, states)
-
-
-print("combined_states_all_sentences: ", combined_states_all_sentences)
-# Iterate over sentences, except the last since we're looking at transitions
-for i, sentence in enumerate(med_context_sentences[:-1]):
-    subject = get_subject_from_event(sentence)  # Extract subject from the sentence
+# # TODO need to implement this with event dict for all levels and category combos now
+# print("combined_states_all_sentences: ", combined_states_all_sentences)
+# # Iterate over sentences, except the last since we're looking at transitions
+# for i, sentence in enumerate(med_context_sentences[:-1]):
+#     subject = get_subject_from_event(sentence)  # Extract subject from the sentence
     
-    # Ensure we have the next state to form a transition
-    if i < len(combined_states_all_sentences) - 1:
-        prev_state = combined_states_all_sentences[i]
-        next_state = combined_states_all_sentences[i + 1]
+#     # Ensure we have the next state to form a transition
+#     if i < len(combined_states_all_sentences) - 1:
+#         prev_state = combined_states_all_sentences[i]
+#         next_state = combined_states_all_sentences[i + 1]
 
-        # Convert states to matrix indices
-        prev_vector = cog_vectors[prev_state]
-        next_vector = cog_vectors[next_state]
+#         # Convert states to matrix indices
+#         prev_vector = cog_vectors[prev_state]
+#         next_vector = cog_vectors[next_state]
 
-        # Update matrices for all hierarchical levels and temporal categories
-        for hierarchy in hierarchy_levels:
-            for temporality in temporality_levels:
-                transition_matrices[subject][hierarchy][temporality][prev_vector, next_vector] += 1
+#         # Update matrices for all hierarchical levels and temporal categories
+#         for hierarchy in hierarchy_levels:
+#             for temporality in temporality_levels:
+#                 transition_matrices[subject][hierarchy][temporality][prev_vector, next_vector] += 1
 
-# After updating the matrices, normalize them and save for persistence
-normalize_matrices(transition_matrices)  # Normalize your matrices as implemented previously
-save_transition_matrices(transition_matrices, 'transition_matrices.npy')  # Save your matrices as implemented previously
+# # After updating the matrices, normalize them and save for persistence
+# normalize_matrices(transition_matrices)  # Normalize your matrices as implemented previously
+# save_transition_matrices(transition_matrices, 'transition_matrices.npy')  # Save your matrices as implemented previously
 
-bp_index = cog_vectors['BP']  # cog_vectors['BP'] should give you the index for 'BP'
-# Print out the matrices to check them
-for char in characters:
-    print(f"Transition matrices for {char} in 'context-specific' 'medium-term':")
-    print(transition_matrices[char]['context-specific']['medium-term'])
-    print(f"Transition from 'BP' for {char} in 'context-specific' 'medium-term':")
-    bp_transitions = transition_matrices[char]['context-specific']['medium-term'][bp_index]
-    print(bp_transitions)
+# bp_index = cog_vectors['BP']  # cog_vectors['BP'] should give you the index for 'BP'
+# # Print out the matrices to check them
+# for char in characters:
+#     print(f"Transition matrices for {char} in 'context-specific' 'medium-term':")
+#     print(transition_matrices[char]['context-specific']['medium-term'])
+#     print(f"Transition from 'BP' for {char} in 'context-specific' 'medium-term':")
+#     bp_transitions = transition_matrices[char]['context-specific']['medium-term'][bp_index]
+#     print(bp_transitions)
 
-# Save the transition matrices if needed
-np.save('bob_matrix.npy', transition_matrices['Bob']['context-specific']['medium-term'])
+# # Save the transition matrices if needed
+# np.save('bob_matrix.npy', transition_matrices['Bob']['context-specific']['medium-term'])
 
-# ... Additional code for loading and using the matrices
-# Example usage for a specific character, hierarchical level, and temporal category
-char = 'Bob'
-hierarchy = 'context-specific'
-temporality = 'medium-term'
+# # ... Additional code for loading and using the matrices
+# # Example usage for a specific character, hierarchical level, and temporal category
+# char = 'Bob'
+# hierarchy = 'context-specific'
+# temporality = 'medium-term'
 
-# Ensure the matrix is normalized
-normalize_matrices(transition_matrices)  # Assuming this normalizes all matrices
+# # Ensure the matrix is normalized
+# normalize_matrices(transition_matrices)  # Assuming this normalizes all matrices
 
-# Get the transition matrix
-transition_matrix = transition_matrices[char][hierarchy][temporality]
+# # Get the transition matrix
+# transition_matrix = transition_matrices[char][hierarchy][temporality]
 
-# Find the stationary distribution
-stationary_distribution = find_stationary_distribution(transition_matrix)
-print(f"Stationary distribution for {char} in {hierarchy} {temporality}: {stationary_distribution}")
+# # Find the stationary distribution
+# stationary_distribution = find_stationary_distribution(transition_matrix)
+# print(f"Stationary distribution for {char} in {hierarchy} {temporality}: {stationary_distribution}")
 
 
 
